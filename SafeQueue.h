c174ee45a6539ff693bbf39e8afff372d1bf2c4b
Rnayt <151448952+Rnayt@users.noreply.github.com>
2023-11-19 22:36:09 +0800

    Update SafeQueue.h

--- SafeQueue.h
+++ SafeQueue.h
@@ -7,35 +7,35 @@ template <typename T>
 class SafeQueue
 {
 public:
-	void Push(T element)
-	{
-		std::unique_lock<std::mutex> lock(_mtx);
-		_queue.push(element);
-		_cond.notify_one();
-	}
+    void Push(T element)
+    {
+	std::unique_lock<std::mutex> lock(_mtx);
+	_queue.push(element);
+	_cond.notify_one();
+    }
          
-	T PopFront()
-	{
-		std::unique_lock<std::mutex> lock(_mtx);
+    T PopFront()
+    {
+     	std::unique_lock<std::mutex> lock(_mtx);
 			
-		_cond.wait(lock, [this]() 
-		{ 
-			return !_queue.empty(); 
-		}); 
+	_cond.wait(lock, [this]() 
+	{ 
+	    return !_queue.empty(); 
+	}); 
 
-		T element = _queue.front();
-		_queue.pop();
+	T element = _queue.front();
+	_queue.pop();
 
-		return element;
-	}
+	return element;
+    }
         
     void Pop()
     {
         std::unique_lock<std::mutex> lock(_mtx);
-		_cond.wait(lock, [this]() 
-		{ 
-			return !_queue.empty(); 
-		}); 
+	_cond.wait(lock, [this]() 
+	{ 
+	    return !_queue.empty(); 
+	}); 
         _queue.pop();
     }
 
@@ -53,8 +53,8 @@ public:
 
     void Dump()
     {
-		std::unique_lock<std::mutex> lock(_mtx);
-		std::cout << " -- queue -- \n";
+	std::unique_lock<std::mutex> lock(_mtx);
+	std::cout << " -- queue -- \n";
         std::queue<T> copy = _queue; 
 
         while (!copy.empty())
@@ -62,7 +62,7 @@ public:
             std::cout << copy.front().second << std::endl;
             copy.pop();
         }
-		std::cout << " -- \n";
+	std::cout << " -- \n";
     }
 
 private:
