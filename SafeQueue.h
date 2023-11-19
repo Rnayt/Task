88ebe2e2f32cef41b4453f697b8d8b4aeb99b9e2
Rnayt <151448952+Rnayt@users.noreply.github.com>
2023-11-19 22:33:27 +0800

    Update SafeQueue.h

--- SafeQueue.h
+++ SafeQueue.h
@@ -6,68 +6,68 @@
 template <typename T>
 class SafeQueue
 {
-	public:
-		void Push(T element)
-		{
-			std::unique_lock<std::mutex> lock(_mtx);
-			_queue.push(element);
-			_cond.notify_one();
-		}
+public:
+	void Push(T element)
+	{
+		std::unique_lock<std::mutex> lock(_mtx);
+		_queue.push(element);
+		_cond.notify_one();
+	}
          
-		T PopFront()
-		{
-			std::unique_lock<std::mutex> lock(_mtx);
+	T PopFront()
+	{
+		std::unique_lock<std::mutex> lock(_mtx);
 			
-			_cond.wait(lock, [this]() 
-			{ 
-				return !_queue.empty(); 
-			}); 
+		_cond.wait(lock, [this]() 
+		{ 
+			return !_queue.empty(); 
+		}); 
 
-			T element = _queue.front();
-			_queue.pop();
+		T element = _queue.front();
+		_queue.pop();
 
-			return element;
-		}
+		return element;
+	}
         
-        void Pop()
+    void Pop()
+    {
+        std::unique_lock<std::mutex> lock(_mtx);
+		_cond.wait(lock, [this]() 
+		{ 
+			return !_queue.empty(); 
+		}); 
+        _queue.pop();
+    }
+
+    T Front()
+    {
+        std::unique_lock<std::mutex> lock(_mtx);
+        T element = _queue.front();
+        return element;
+    }
+
+    int Empty()
+    {
+        return _queue.empty();
+    }
+
+    void Dump()
+    {
+		std::unique_lock<std::mutex> lock(_mtx);
+		std::cout << " -- queue -- \n";
+        std::queue<T> copy = _queue; 
+
+        while (!copy.empty())
         {
-            std::unique_lock<std::mutex> lock(_mtx);
-			_cond.wait(lock, [this]() 
-			{ 
-				return !_queue.empty(); 
-			}); 
-            _queue.pop();
+            std::cout << copy.front().second << std::endl;
+            copy.pop();
         }
+		std::cout << " -- \n";
+    }
 
-        T Front()
-        {
-            std::unique_lock<std::mutex> lock(_mtx);
-            T element = _queue.front();
-            return element;
-        }
+private:
+ 	std::queue<T> _queue;
+	std::mutex _mtx;
 
-        int Empty()
-        {
-            return _queue.empty();
-        }
-
-        void Dump()
-        {
-			std::unique_lock<std::mutex> lock(_mtx);
-			std::cout << " -- queue -- \n";
-            std::queue<T> copy = _queue; 
-
-            while (!copy.empty())
-            {
-                std::cout << copy.front().second << std::endl;
-                copy.pop();
-            }
-			std::cout << " -- \n";
-        }
-
-	private:
-		std::queue<T> _queue;
-		std::mutex _mtx;
-
-		std::condition_variable _cond;
+	std::condition_variable _cond;
 };
